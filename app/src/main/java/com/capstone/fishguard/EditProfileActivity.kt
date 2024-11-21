958b52cd3cf55b363db255c946555ff2c46af8e6
Adhityandi <adhityandianggara.22008@mhs.unesa.ac.id>
2024-11-21 18:45:25 +0700

    initial commit

---
+++ app/src/main/java/com/capstone/fishguard/EditProfileActivity.kt
@@ -0,0 +1,4 @@
+package com.capstone.fishguard
+
+class EditProfileActivity {
+}
---
+++ app/src/main/java/com/capstone/fishguard/ui/profile/ProfileFragmentViewModel.kt
@@ -0,0 +1,7 @@
+package com.capstone.fishguard.ui.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileFragmentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
